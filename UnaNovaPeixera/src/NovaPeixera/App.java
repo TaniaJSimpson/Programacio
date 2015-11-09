044b8c599db529ec0eedfc2707ed53687f05b8ee
Tania <litvinenkotania@gmail.com>
2015-11-09 19:00:11 +0100

    Afegida Imatge del peix. Cal revisar.

--- UnaNovaPeixera/src/NovaPeixera/App.java
+++ UnaNovaPeixera/src/NovaPeixera/App.java
@@ -1,5 +1,6 @@
 package NovaPeixera;
 
+import acm.graphics.GImage;
 import acm.program.GraphicsProgram;
 
 public class App extends GraphicsProgram {
@@ -8,8 +9,16 @@ public class App extends GraphicsProgram {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	private static final int MIDAPEIXERA=600;
 
 	public void run(){
+		Peixera peixera= new Peixera();
+		
+		for(int i=0; i<10; i++){
+			GImage imatge= new GImage("peix1.png");
+			add(imatge);
+		}
+		
 		
 	}
 
